cec04bb8ea7843ee871556631aa4d5f6f9c76a7b
Paola Colina <pacolinam@ul.edu.co>
2019-08-29 08:28:12 -0500

    Iniciando proyecto Madicalsoft

---
+++ src/medicalsoft/Medicalsoft.java
@@ -0,0 +1,17 @@
+
+package medicalsoft;
+
+
+public class Medicalsoft {
+
+    
+    public static void main(String[] args) {
+       
+      Persona PrimerMedico= new Persona("luis", "Martinez");
+        
+        System.out.println(PrimerMedico.getNombre());
+        System.out.println(PrimerMedico.getApellido());
+        
+    }
+    
+}
---
+++ src/medicalsoft/Persona.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medicalsoft;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Persona {
+     private  String nombre ="";
+  private String apellido ="";
+  private String fecha_nacimiento ="";
+  private String identificaccion="";
+  private String genero ="";
+  private String direccion="";
+  private  String email ="";
+  private String telefono ="";
+  private String celular ="";
+
+    public Persona(String nom, String ape) {
+        this.nombre=nom;
+        this.apellido=ape;
+    }
+
+    public String getNombre() {
+        return nombre.toLowerCase();
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getIdentificaccion() {
+        return identificaccion;
+    }
+
+    public void setIdentificaccion(String identificaccion) {
+        this.identificaccion = identificaccion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+}
